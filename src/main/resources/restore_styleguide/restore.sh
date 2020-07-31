06e15e5240e9100415bcb0170ab94fe9ebac9b37
shilpaurandoor <shilpaurandoor@gmail.com>
2020-07-31 15:26:21 +0530

    commiting few checks on source,recovery, vnet

--- src/main/resources/restore_styleguide/restore.sh
+++ src/main/resources/restore_styleguide/restore.sh
@@ -29,16 +29,41 @@ else
 	err " Subscription could not set successfully" && exit
 fi
 
-# 3. restore postgres database server
+#3 check if source server exists. if not terminate the script
+echo "checking if source server exists"
+if az postgres server show --name "$az_source_server" --resource-group "$az_resource_group"; then
+	echo " source server exists"
+else
+	err " source server does not exist" && exit
+fi
+
+#4 check if recovery server exists. If yes delete it
+echo "checking for recovery db details"
+if az postgres server show --name "$az_recovery_server" --resource-group "$az_resource_group"; then
+	if az postgres server delete --resource-group "$az_resource_group" --name "$az_recovery_server"; then
+		echo " recovery database deletion successful" && sleep 500
+	else
+		err " unable to delete the existing recovery server" && exit
+fi
+
+#5 check if virtual network exists or not. if not terminate the script
+echo "checking for virtual network"
+if az network vnet show -g "$az_shoot_resource" -n "$az_shoot_resource"; then
+	echo " virtual network exists"
+else
+	err " required virtual network to access the resource does not exist" && exit
+fi
+
+
+# 6. restore postgres database server
 echo "restoring postgresql database..."
-echo "az postgres server restore --resource-group $az_resource_group --name $az_recovery_server  --restore-point-in-time $az_restore_time --source-server $az_source_server"
 if az postgres server restore --resource-group "$az_resource_group" --name "$az_recovery_server"  --restore-point-in-time "$az_restore_time" --source-server "$az_source_server"; then
 	echo " postgresql database has restored successfully" && sleep 60
 else
 	err " Error occured while restoring database" && exit
 fi
 
-# 4. add virtual network rules to the restored database server
+# 7. add virtual network rules to the restored database server
 echo "adding vnet rules"
 if az postgres server vnet-rule create --resource-group "$az_resource_group" --server-name "$az_recovery_server" --name "$az_vnetrulename" --subnet /subscriptions/"$az_subscription"/resourceGroups/"$az_shoot_resource"/providers/Microsoft.Network/virtualNetworks/"$az_shoot_resource"/subnets/"$az_subnet"; then
 	echo " virtualNetworks has been added successfully for recovered postgres database server" && sleep 60
@@ -46,7 +71,7 @@ else
 	err " Error occured while adding vnet rules" && exit
 fi
 
-# 5. test connection to the restored server 
+# 8. test connection to the restored server 
 echo "making a call to recovery db"
 query1="psql -h $az_host_recovery -U $az_user_recovery -d postgres -c "select name from "admin_users" where name = 'root'""
 if recovery_name=$(PGPASSWORD=$az_password $query1); then
@@ -55,7 +80,7 @@ else
 	err " DB call unsuccessful" && exit
 fi
 
-# 6. delete the original database server
+# 9. delete the original database server
 echo "deleting original instance"
 if az postgres server delete -y --resource-group "$az_resource_group" --name "$az_source_server" --subscription "$az_subscription"; then
 	echo " original instance has been deleted successfully" && sleep 500
@@ -63,7 +88,7 @@ else
 	err " original instance deletion failed" && exit
 fi
 
-# 7. restored to original database server from the recovered server
+# 10. restored to original database server from the recovered server
 echo "restoring to original database..."
 currenttime=$(date -u +%Y-%m-%dT%H:%M:%S.000Z);
 if az postgres server restore --resource-group "$az_resource_group" --name "$az_source_server"  --restore-point-in-time "$currenttime" --source-server "$az_recovery_server";then
@@ -72,7 +97,7 @@ else
 	err " original database deletion failed" && exit
 fi
 
-# 8. add virtual network rule to the original database server
+# 11. add virtual network rule to the original database server
 echo "adding vnet rules to restored original database"
 if az postgres server vnet-rule create --resource-group "$az_resource_group" --server-name "$az_source_server" --name "$az_vnetrulename" --subnet /subscriptions/"$az_subscription"/resourceGroups/"$az_shoot_resource"/providers/Microsoft.Network/virtualNetworks/"$az_shoot_resource"/subnets/"$az_subnet"; then
 	echo " virtualNetworks has been added successfully for original postgres database server" && sleep 60
@@ -80,7 +105,7 @@ else
 	err " Error occured while adding vnet rules for original database server" && exit
 fi
 
-# 9. test connection to the original database server 
+# 12. test connection to the original database server 
 echo "making a call to original db"
 query2="psql -h $az_host -U $az_user -d postgres -c "select name from "admin_users" where name = 'root'""
 if name=$(PGPASSWORD=$az_password $query2); then
@@ -89,10 +114,10 @@ else
 	err " DB call to original server unsuccessfull" && exit
 fi
 
-# 10. delete the recovered database server which was used to restored original server
+# 13. delete the recovered database server which was used to restored original server
 echo "deleting recovery instance"
 if az postgres server delete -y --resource-group "$az_resource_group" --name "$az_recovery_server" --subscription "$az_subscription"; then
-	echo " recovered instance has been deleted successfully" && sleep 60
+	echo " recovered instance has been deleted successfully"
 else
 	err " recovered instance deletion failed" && exit
 fi
